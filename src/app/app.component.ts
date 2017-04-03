9495df52aa220e68703b98771b28e779bfaadb01
jaxkodex <jorge12745@gmail.com>
2017-04-02 23:25:25 -0500

    visual enhence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  showMenu;
+
+  constructor(){
+    this.showMenu = false;
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
 }
